3128aa84ca23678d5b23a5f2e34574b05aee8959
ebrahim2355 <web.ebrahimali@gmail.com>
2025-11-09 02:59:31 +0600

    react app created

---
+++ src/main.jsx
@@ -0,0 +1,29 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router'
+import Root from './layouts/Root.jsx'
+import Home from './components/Home/Home.jsx'
+import AuthProvider from './contexts/AuthProvider.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: Root,
+    children: [
+      {
+        index: true,
+        Component: Home,
+      }
+    ]
+  }
+])
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <AuthProvider>
+      <RouterProvider router={router}></RouterProvider>
+    </AuthProvider>
+  </StrictMode>,
+)
