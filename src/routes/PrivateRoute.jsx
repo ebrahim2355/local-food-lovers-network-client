1e8f8fcc3fcc2077475d89687dfbde31d9ad6943
ebrahim2355 <web.ebrahimali@gmail.com>
2026-01-15 20:04:34 +0600

    polished

--- src/routes/PrivateRoute.jsx
+++ src/routes/PrivateRoute.jsx
@@ -1,22 +1,32 @@
-import React, { use } from 'react';
-import { AuthContext } from '../contexts/AuthContext';
-import { Navigate, useLocation } from 'react-router';
+import React, { use } from "react";
+import { AuthContext } from "../contexts/AuthContext";
+import { Navigate, useLocation } from "react-router";
 
 const PrivateRoute = ({ children }) => {
     const { user, loading } = use(AuthContext);
-
     const location = useLocation();
-    // console.log(location);
 
     if (loading) {
-        return <span className='loading loading-spinner text-success'></span>
+        return (
+            <div className="min-h-screen flex flex-col items-center justify-center gap-4">
+                <span className="loading loading-spinner loading-lg text-orange-500"></span>
+                <p className="text-sm text-gray-500 dark:text-gray-400">
+                    Loading. Please wait…
+                </p>
+            </div>
+        );
     }
 
     if (user) {
         return children;
     }
+
     return (
-        <Navigate state={location?.pathname} to={"/login"}></Navigate>
+        <Navigate
+            to="/login"
+            state={{ from: location.pathname }}
+            replace
+        />
     );
 };
 
